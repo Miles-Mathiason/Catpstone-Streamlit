186e4493e51506511f2381fc36e3075c9f42a3cf
Miles-Mathiason <156451734+Miles-Mathiason@users.noreply.github.com>
2024-03-25 11:14:12 +0000

    Update capstone_streamlit.py

--- capstone_streamlit.py
+++ capstone_streamlit.py
@@ -8,12 +8,17 @@ df = conn.query('SELECT * FROM student.mm_artists;', ttl="10m")
 
 # Making week:artist dict
 ordered_weeks = tuple(set(df['week_']))
-artist_dict = {}
 
+artist_dict = {}
 for week in ordered_weeks:
   artists = []
   for i in range(0,len(df)):
-    f'{df.loc[i]["week_"]}'
+    if df.loc[i]["week_"] == week:
+      artists += [df.loc[i]["artist"]
+  artist_dict[week] = artists
+
+f'{artist_dict}'
+      
     
   
 
