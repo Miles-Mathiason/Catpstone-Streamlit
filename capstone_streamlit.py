7fe4540c43d338e2535963b94285a4f979e033e9
Miles-Mathiason <156451734+Miles-Mathiason@users.noreply.github.com>
2024-03-25 17:04:26 +0000

    Update capstone_streamlit.py

--- capstone_streamlit.py
+++ capstone_streamlit.py
@@ -120,7 +120,7 @@ col1, col2 = st.columns(2)
 col1.metric("Number of Entering Artists", f'#{entering_artists_count}')
 col2.metric("Number of Remaining Artists", f'#{defending_artists_count}')
 
-st.bar_chart(artist_count_dict, width=1)
+#st.bar_chart(artist_count_dict, width=1)
 
 ## Artist breakdown
 artists_set = set(df['artist'])
