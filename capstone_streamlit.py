e246adf9f0d0332a8469abb9f30ae09c857877e2
Miles-Mathiason <156451734+Miles-Mathiason@users.noreply.github.com>
2024-03-25 11:07:45 +0000

    Update capstone_streamlit.py

--- capstone_streamlit.py
+++ capstone_streamlit.py
@@ -6,9 +6,16 @@ conn = st.connection("postgresql", type="sql")
 # Perform query.
 df = conn.query('SELECT * FROM student.mm_artists;', ttl="10m")
 
-
+# Making week:artist dict
 ordered_weeks = tuple(set(df['week_']))
+artist_dict = {}
 
+for week in ordered_weeks:
+  artists = []
+  for i in range(0,len(df)):
+    f'{i}'
+    
+  
 
 
 
@@ -21,4 +28,3 @@ ordered_weeks = tuple(set(df['week_']))
 st.title('UK Charts Artist Breakdown with Spotify Data')
 st.dataframe(df)
 
-f'{ordered_weeks}'
