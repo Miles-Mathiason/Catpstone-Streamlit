1eb5b8d6ef447b3967ae45b5755dd731c1960f61
Miles-Mathiason <156451734+Miles-Mathiason@users.noreply.github.com>
2024-03-25 10:59:37 +0000

    Update capstone_streamlit.py

--- capstone_streamlit.py
+++ capstone_streamlit.py
@@ -6,7 +6,7 @@ conn = st.connection("postgresql", type="sql")
 # Perform query.
 df = conn.query('SELECT * FROM student.mm_artists;', ttl="10m")
 
-
+artist_list = df['artist']
 
 
 
@@ -21,4 +21,4 @@ st.title('UK Charts Artist Breakdown with Spotify Data')
 st.dataframe(df)
 
 
-f'{[1,2,3]}'
+f'{artist_list}'
